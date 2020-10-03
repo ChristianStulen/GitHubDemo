0d56fd21f69ee12289f754a96be7df1d0407302b
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2020-10-03 10:45:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    // Välkommen till Github
+        // Du har nu laddat ner ett Java-projekt
+        // via IntelliJ IDEA.
+
+        // Lycka till med Git och GitHub
+        // Mahmud Al Hakim
+    }
+}
